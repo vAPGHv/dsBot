f8cb9be45b3e974c1d9b38704a1760313f888f70
Andrew <97779449+vAPGHv@users.noreply.github.com>
2022-07-21 16:49:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,35 +4,28 @@ import requests as req
 from bs4 import BeautifulSoup as BS
 from settings import *
 import random
+from getimg import getimg
 
 
 class MyClient(discord.Client):
     async def on_message(self, message):
-        lowerm = message.content.lower()
+        try:
+            lowerm = message.content.lower()
 
-        for m in cens:
-            if m in lowerm:
-                await message.delete()
+            for m in cens:
+                if m in lowerm:
+                    await message.delete()
 
-        if '/img' in lowerm:
-            reg = message.content.split("/img ")[1]  # get the word after the command
-            url = f"https://www.google.com/search?q={reg}&client=opera&hs=nkI&hl=ru&sxsrf=APq-WBuUWCpyLYYduHWl9vqF9dG_IIvdpg:1649445742756&source=lnms&tbm=isch&sa=X&ved=2ahUKEwizzsqcmIX3AhVM-yoKHcKBCg0Q_AUoAXoECAEQAw&biw=2519&bih=1299&dpr=1"
+            if '/img' in lowerm[0:4]:
+                reg = message.content.split("/img ", maxsplit=1)[1]  # get the word after the command
+                print(reg)
+                getimg(reg)
 
-            r = req.get(url)
-
-            soup = BS(r.content, "html.parser")
-            list1 = []
-
-            for i in soup.select("img", class_="Q4LuWd"):
-                list1.append(i.get("src"))
-
-            randel = list1[random.randrange(1, len(list1))]
-            randel = req.get(randel).content
-
-            with open(f"123.jpg", "wb") as file:
-                file.write(randel)
-
-            await message.reply(file=discord.File('123.jpg'))
+                await message.reply(file=discord.File('img.jpg'))
+        except Exception as ex:
+            await message.reply(ex)
+            print(ex)
+            return ex
 
 
 client = MyClient()
